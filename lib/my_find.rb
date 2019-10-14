45b89f080452a022a28849860f8b7a6c32baf7c2
Andrew Capp <acapp909@gmail.com>
2019-10-14 17:21:43 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,12 @@
 require 'pry'
 
 def my_find(collection)
+  i = 0
+  find_return_array = []
 
+  while i < collection.length
+    find_return_array << yield(collection[i])
+    i += 1
+  end
+  
 end
